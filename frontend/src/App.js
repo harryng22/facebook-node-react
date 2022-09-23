d63192c6dcde0a5c5cd0671442ac87800e1d90e8
Hieu Nguyen <hieunguyen@Anhs-MacBook-Pro.local>
2022-09-23 23:08:14 +0700

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { FriendsActive, LiveVideo } from "./svg";
+
+function App() {
+  return <div>
+    welcome to frontend
+    <FriendsActive />
+    <LiveVideo color="red" />
+  </div>;
+}
+
+export default App;
