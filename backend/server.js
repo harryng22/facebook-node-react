13244c0fe0906f58f88316cd6399bcdfc1ffe217
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-09-24 17:43:12 +0700

    gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,16 @@
 import express from "express";
 
 const app = express();
+const PORT = 8000;
+
+app.get('/', (req,res)=>{
+    res.send('Welcome home')
+})
+
+app.get('/books', (req,res)=>{
+    res.send('Welcome to books')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at port ${PORT}`);
+});
