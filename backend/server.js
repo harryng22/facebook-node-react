90097aa14da231fda40f5d21800fef5c413a7587
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-09-26 15:17:38 +0000

    26 Sept 2022, 10:17 pm

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import userRoutes from "./routes/user";
+import userRoutes from "./routes/user.js";
 
 const app = express();
 const PORT = 8000;
