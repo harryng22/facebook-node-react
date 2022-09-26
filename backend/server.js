33701188e07fbd2825a4a947293ea3cf1ac0ebe1
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-09-26 14:37:36 +0000

    26 Sept 2022, 9:37 pm

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
 
-app.use()
+const options = {
+  origin: "https://localhost:3000",
+  useSuccessStatus: 200,
+};
 
-app.get('/', (req,res)=>{
-    res.send('Welcome home')
-})
+app.use(cors(options));
 
-app.get('/books', (req,res)=>{
-    res.send('Welcome to books')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome home");
+});
+
+app.get("/books", (req, res) => {
+  res.send("Welcome to books");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}`);
