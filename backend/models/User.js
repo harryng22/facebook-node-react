78e526b4a4105e65135f232e616f9e0a66d18aa4
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-09-27 16:29:03 +0000

    27 Sept 2022, 11:29 pm

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,136 +2,141 @@ import mongoose from "mongoose";
 
 const { ObjectId } = mongoose.Schema;
 
-const userSchema = mongoose.Schema({
-  first_name: {
-    type: String,
-    required: [true, "First name is required"],
-    trim: true,
-    text: true,
-  },
-  last_name: {
-    type: String,
-    required: [true, "Last name is required"],
-    trim: true,
-    text: true,
-  },
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-    trim: true,
-    text: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Email is required"],
-  },
-  picture: {
-    type: String,
-    trim: true,
-    default:
-      "https://gravatar.com/avatar/16e7992c7357bd9b986f3c45578c72db?s=400&d=mp&r=x",
-  },
-  cover: {
-    type: String,
-    trim: true,
-  },
-  gender: {
-    type: String,
-    required: [true, "Gender is required"],
-    trim: true,
-  },
-  birthYear: {
-    type: Number,
-    required: true,
-    trim: true,
-  },
-  birthMonth: {
-    type: Number,
-    required: true,
-    trim: true,
-  },
-  birthDay: {
-    type: Number,
-    required: true,
-    trim: true,
-  },
-  verified: {
-    type: Boolean,
-    default: false,
-  },
-  friends: {
-    type: Array,
-    default: [],
-  },
-  following: {
-    type: Array,
-    default: [],
-  },
-  followers: {
-    type: Array,
-    default: [],
-  },
-  requests: {
-    type: Array,
-    default: [],
-  },
-  search: [
-    {
-      user: {
-        type: ObjectId,
-        ref: "User",
+export const userSchema = mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: [true, "First name is required"],
+      trim: true,
+      text: true,
+    },
+    last_name: {
+      type: String,
+      required: [true, "Last name is required"],
+      trim: true,
+      text: true,
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      trim: true,
+      text: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    picture: {
+      type: String,
+      trim: true,
+      default:
+        "https://gravatar.com/avatar/16e7992c7357bd9b986f3c45578c72db?s=400&d=mp&r=x",
+    },
+    cover: {
+      type: String,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      required: [true, "Gender is required"],
+      trim: true,
+    },
+    birthYear: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    birthMonth: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    birthDay: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    requests: {
+      type: Array,
+      default: [],
+    },
+    search: [
+      {
+        user: {
+          type: ObjectId,
+          ref: "User",
+        },
+      },
+    ],
+    detail: {
+      bio: {
+        type: String,
+      },
+      otherName: {
+        type: String,
+      },
+      job: {
+        type: String,
+      },
+      workplace: {
+        type: String,
+      },
+      highSchool: {
+        type: String,
+      },
+      college: {
+        type: String,
+      },
+      currentCity: {
+        type: String,
+      },
+      hometown: {
+        type: String,
+      },
+      relationship: {
+        type: String,
+        enum: ["Single", "In a relationship", "Married"],
+      },
+      instagram: {
+        type: String,
       },
     },
-  ],
-  detail: {
-    bio: {
-      type: String,
-    },
-    otherName: {
-      type: String,
-    },
-    job: {
-      type: String,
-    },
-    workplace: {
-      type: String,
-    },
-    highSchool: {
-      type: String,
-    },
-    college: {
-      type: String,
-    },
-    currentCity: {
-      type: String,
-    },
-    hometown: {
-      type: String,
-    },
-    relationship: {
-      type: String,
-      enum: ["Single", "In a relationship", "Married"],
-    },
-    instagram: {
-      type: String,
-    },
+    savedPosts: [
+      {
+        post: {
+          type: ObjectId,
+          ref: "Post",
+        },
+        savedAt: {
+          type: Date,
+          default: new Date(),
+        },
+      },
+    ],
   },
-  savedPosts: [
-    {
-      post: {
-        type: ObjectId,
-        ref: "Post",
-      },
-      savedAt: {
-        type: Date,
-        default: new Date(),
-      },
-    },
-  ],
-});
+  {
+    timestamps: true,
+  }
+);
