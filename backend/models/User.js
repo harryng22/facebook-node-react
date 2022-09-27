5af4193e71d017a62600d3edca5fb237b364bf45
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-09-27 16:20:01 +0000

    27 Sept 2022, 11:20 pm

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,91 +1,118 @@
 import mongoose from "mongoose";
 
-const {ObjectId} = mongoose.Schema;
+const { ObjectId } = mongoose.Schema;
 
 const userSchema = mongoose.Schema({
-    first_name: {
-        type: String,
-        required: [true, "First name is required"],
-        trim: true,
-        text: true
+  first_name: {
+    type: String,
+    required: [true, "First name is required"],
+    trim: true,
+    text: true,
+  },
+  last_name: {
+    type: String,
+    required: [true, "Last name is required"],
+    trim: true,
+    text: true,
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    text: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Email is required"],
+  },
+  picture: {
+    type: String,
+    trim: true,
+    default:
+      "https://gravatar.com/avatar/16e7992c7357bd9b986f3c45578c72db?s=400&d=mp&r=x",
+  },
+  cover: {
+    type: String,
+    trim: true,
+  },
+  gender: {
+    type: String,
+    required: [true, "Gender is required"],
+    trim: true,
+  },
+  birthYear: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  birthMonth: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  birthDay: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  friends: {
+    type: Array,
+    default: [],
+  },
+  following: {
+    type: Array,
+    default: [],
+  },
+  followers: {
+    type: Array,
+    default: [],
+  },
+  requests: {
+    type: Array,
+    default: [],
+  },
+  search: [
+    {
+      user: {
+        type: ObjectId,
+        ref: "User",
+      },
     },
-    last_name: {
-        type: String,
-        required: [true, "Last name is required"],
-        trim: true,
-        text: true
+  ],
+  detail: {
+    bio: {
+      type: String,
     },
-    username: {
-        type: String,
-        required: [true, "Username is required"],
-        trim: true,
-        text: true,
-        unique: true
+    bio: {
+      type: String,
     },
-    email: {
-        type: String,
-        required: [true, "Email is required"],
-        trim: true
+    bio: {
+      type: String,
     },
-    password: {
-        type: String,
-        required: [true, "Email is required"]
+    bio: {
+      type: String,
     },
-    picture: {
-        type: String,
-        trim: true,
-        default: "https://gravatar.com/avatar/16e7992c7357bd9b986f3c45578c72db?s=400&d=mp&r=x"
+    bio: {
+      type: String,
     },
-    cover: {
-        type: String,
-        trim: true
+    bio: {
+      type: String,
     },
-    gender: {
-        type: String,
-        required: [true, "Gender is required"],
-        trim: true
+    bio: {
+      type: String,
     },
-    birthYear: {
-        type: Number,
-        required: true,
-        trim: true
+    bio: {
+      type: String,
     },
-    birthMonth: {
-        type: Number,
-        required: true,
-        trim: true
-    },
-    birthDay: {
-        type: Number,
-        required: true,
-        trim: true
-    },
-    verified: {
-        type: Boolean,
-        default: false
-    },
-    friends: {
-        type: Array,
-        default: []
-    },
-    following: {
-        type: Array,
-        default: []
-    },
-    followers: {
-        type: Array,
-        default: []
-    },
-    requests: {
-        type: Array,
-        default: []
-    },
-    search: [
-        {
-            user:{
-                type: ObjectId,
-                ref: 'User'
-            }
-        }
-    ]
-})
+  },
+});
