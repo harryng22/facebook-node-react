9ee3c7e0f094eb4d32bef38eaafe22f8c59360ae
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-09-30 16:09:11 +0000

    30 Sept 2022, 11:09 pm

--- backend/models/User.js
+++ backend/models/User.js
@@ -16,125 +16,125 @@ const userSchema = mongoose.Schema(
       trim: true,
       text: true,
     },
-    // username: {
-    //   type: String,
-    //   required: [true, "Username is required"],
-    //   trim: true,
-    //   text: true,
-    //   unique: true,
-    // },
-    // email: {
-    //   type: String,
-    //   required: [true, "Email is required"],
-    //   trim: true,
-    // },
-    // password: {
-    //   type: String,
-    //   required: [true, "Email is required"],
-    // },
-    // picture: {
-    //   type: String,
-    //   trim: true,
-    //   default:
-    //     "https://gravatar.com/avatar/16e7992c7357bd9b986f3c45578c72db?s=400&d=mp&r=x",
-    // },
-    // cover: {
-    //   type: String,
-    //   trim: true,
-    // },
-    // gender: {
-    //   type: String,
-    //   required: [true, "Gender is required"],
-    //   trim: true,
-    // },
-    // birthYear: {
-    //   type: Number,
-    //   required: true,
-    //   trim: true,
-    // },
-    // birthMonth: {
-    //   type: Number,
-    //   required: true,
-    //   trim: true,
-    // },
-    // birthDay: {
-    //   type: Number,
-    //   required: true,
-    //   trim: true,
-    // },
-    // verified: {
-    //   type: Boolean,
-    //   default: false,
-    // },
-    // friends: {
-    //   type: Array,
-    //   default: [],
-    // },
-    // following: {
-    //   type: Array,
-    //   default: [],
-    // },
-    // followers: {
-    //   type: Array,
-    //   default: [],
-    // },
-    // requests: {
-    //   type: Array,
-    //   default: [],
-    // },
-    // search: [
-    //   {
-    //     user: {
-    //       type: ObjectId,
-    //       ref: "User",
-    //     },
-    //   },
-    // ],
-    // detail: {
-    //   bio: {
-    //     type: String,
-    //   },
-    //   otherName: {
-    //     type: String,
-    //   },
-    //   job: {
-    //     type: String,
-    //   },
-    //   workplace: {
-    //     type: String,
-    //   },
-    //   highSchool: {
-    //     type: String,
-    //   },
-    //   college: {
-    //     type: String,
-    //   },
-    //   currentCity: {
-    //     type: String,
-    //   },
-    //   hometown: {
-    //     type: String,
-    //   },
-    //   relationship: {
-    //     type: String,
-    //     enum: ["Single", "In a relationship", "Married"],
-    //   },
-    //   instagram: {
-    //     type: String,
-    //   },
-    // },
-    // savedPosts: [
-    //   {cl
-    //     post: {
-    //       type: ObjectId,
-    //       ref: "Post",
-    //     },
-    //     savedAt: {
-    //       type: Date,
-    //       default: new Date(),
-    //     },
-    //   },
-    // ],
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      trim: true,
+      text: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    picture: {
+      type: String,
+      trim: true,
+      default:
+        "https://gravatar.com/avatar/16e7992c7357bd9b986f3c45578c72db?s=400&d=mp&r=x",
+    },
+    cover: {
+      type: String,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      required: [true, "Gender is required"],
+      trim: true,
+    },
+    birthYear: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    birthMonth: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    birthDay: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    requests: {
+      type: Array,
+      default: [],
+    },
+    search: [
+      {
+        user: {
+          type: ObjectId,
+          ref: "User",
+        },
+      },
+    ],
+    detail: {
+      bio: {
+        type: String,
+      },
+      otherName: {
+        type: String,
+      },
+      job: {
+        type: String,
+      },
+      workplace: {
+        type: String,
+      },
+      highSchool: {
+        type: String,
+      },
+      college: {
+        type: String,
+      },
+      currentCity: {
+        type: String,
+      },
+      hometown: {
+        type: String,
+      },
+      relationship: {
+        type: String,
+        enum: ["Single", "In a relationship", "Married"],
+      },
+      instagram: {
+        type: String,
+      },
+    },
+    savedPosts: [
+      {
+        post: {
+          type: ObjectId,
+          ref: "Post",
+        },
+        savedAt: {
+          type: Date,
+          default: new Date(),
+        },
+      },
+    ],
   },
   {
     timestamps: true,
