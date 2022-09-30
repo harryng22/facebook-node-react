7fb5b4dabaa65a17ce8755e0e524f2792026c1f0
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-09-30 15:55:23 +0000

    30 Sept 2022, 10:55 pm

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,11 @@
-export const register = (req,res) => {
-    
-}
+import User from "../models/User.js";
+
+export const register = async (req, res) => {
+  try {
+    const user = await new User(req.body).save();
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
